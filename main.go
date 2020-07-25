d879cf326c71bf6848ce3d61c164144ed7c86580
simbathesailor <anilchaudhary453@gmail.com>
2020-07-25 22:35:02 +0530

    fix

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	t:= "Hello World"
+
+	t := "Hello World"
 	fmt.Println("t is", t)
+
+	// Handle the /hello request get request
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello!")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
